2ebfc0b3ead8e5ce8ffc1184e70a93c62a97b54b
KGCODING <86546826+KGGAMINGG@users.noreply.github.com>
2022-02-20 10:40:49 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload() {
+
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('I_have_a_mustache.png');
+}
+
